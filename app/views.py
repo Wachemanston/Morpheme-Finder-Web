4ce6e8bfc300dab1cb3bd107b81c0e893d39fcfe
ArrmenLee <wachemanstonphoenix@gmail.com>
2020-10-19 15:04:01 +0800

    (Template)[A] Index page

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Success!")
+    return render(request, 'index.html', {
+        'var': 'success',
+    })
